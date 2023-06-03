08231826dded2ca6eda889851f20ff46da386c71
Nathan <pythoniszen@gmail.com>
2023-06-02 20:23:08 -0700

    6/2/23 Finish the rest of the logic for the main functions of the game

--- script.js
+++ script.js
@@ -4,124 +4,111 @@ let tieRecord = 0;
 
 game();
 
+// Triggers function chain to run the game loop.
 function game() {
-    
-    let roundResult = '';
+  const options = [
+    { id: 'rock' },
+    { id: 'paper' },
+    { id: 'scissors' }
+  ];
 
-    const rock = document.querySelector('#rock');
-        
-    rock.addEventListener('click', () => {
-        roundResult = playRound(rock.id);
-        console.log(roundResult);
-
-        if (roundResult.charAt(4) === 'W') {
-            roundResult = 'Player Wins';
-        } else if (roundResult.charAt(4) === 'L') {
-            roundResult = 'Computer Wins';
-        } else {
-            roundResult = 'Tie';
-        }
-
-        if (roundResult === 'Player Wins') {
-            playerScore++;
-            console.log(playerScore);
-            let playerScoreDisplay = document.querySelector('#playerScore');
-            playerScoreDisplay.innerHTML = "Player: " + playerScore;
-            checkScore();
-        } else if (roundResult === 'Computer Wins') {
-            computerScore++;
-            let computerScoreDisplay = document.querySelector('#computerScore');
-            computerScoreDisplay.innerHTML = "Computer: " + computerScore;
-            checkScore();
-        } else if (roundResult === 'Tie') {
-            tieRecord++;
-            let tieRecordDisplay = document.querySelector('#tieRecord');
-            tieRecordDisplay.innerHTML = "Rounds Tied: " + tieRecord;
-        }
+  options.forEach(option => {
+    const button = document.querySelector(`#${option.id}`);
+    button.addEventListener('click', () => {
+      computeRoundResult(button);
     });
+  });
 }
 
+// Helper functions:
+
+// Takes in the button clicked and passes it to the playRound function.
+// Then, it takes the result of the playRound function and displays it on the screen.
+function computeRoundResult(buttonClicked) {
+  let roundResult = playRound(buttonClicked.id);
+
+  if (roundResult.charAt(4) === 'W') {
+    roundResult = 'Player Wins';
+    playerScore++;
+  } else if (roundResult.charAt(4) === 'L') {
+    roundResult = 'Computer Wins';
+    computerScore++;
+  } else {
+    roundResult = 'Tie';
+    tieRecord++;
+  }
+
+  updateScoreDisplay();
+  checkScore();
+}
+
+// Computes choice for computer and compares it to the player's choice.
 function playRound(elementId) {
-    let playerChoice = elementId.toUpperCase();
-    let computerChoice = getComputerChoice();
-    let result = '';
-    if (playerChoice === computerChoice) {
-        return result = 'Tie';
-    } else if (playerChoice === 'ROCK') {
-        if (computerChoice === 'PAPER') {
-            result = 'You Lose! Paper beats Rock';
-        } else {
-            result = 'You Win! Rock beats Scissors';
-        }
-    } else if (playerChoice === 'PAPER') {
-        if (computerChoice === 'SCISSORS') {
-            result = 'You Lose! Scissors beats Paper';
-        } else {
-            result = 'You Win! Paper beats Rock';
-        }
-    } else if (playerChoice === 'SCISSORS') {
-        if (computerChoice === 'ROCK') {
-            result = 'You Lose! Rock beats Scissors';
-        } else {
-            result = 'You Win! Scissors beats Paper';
-        }
-    }
+  const playerChoice = elementId.toUpperCase();
+  const computerChoice = getComputerChoice();
+  let result = '';
 
-    return result;
+  if (playerChoice === computerChoice) {
+    result = 'Tie';
+  } else if (playerChoice === 'ROCK') {
+    result = computerChoice === 'PAPER' ? 'You Lose! Paper beats Rock' : 'You Win! Rock beats Scissors';
+  } else if (playerChoice === 'PAPER') {
+    result = computerChoice === 'SCISSORS' ? 'You Lose! Scissors beats Paper' : 'You Win! Paper beats Rock';
+  } else if (playerChoice === 'SCISSORS') {
+    result = computerChoice === 'ROCK' ? 'You Lose! Rock beats Scissors' : 'You Win! Scissors beats Paper';
+  }
 
-    
+  return result;
 }
-
 
 function getComputerChoice() {
-    let choiceValue = Math.floor(Math.random() * 100);
-    if (choiceValue <= 33) {
-        return 'ROCK';
-    } else if (choiceValue <= 66) {
-        return 'PAPER';
-    } else {
-        return 'SCISSORS';
-    }
+  const choiceValue = Math.floor(Math.random() * 100);
+
+  if (choiceValue <= 33) {
+    return 'ROCK';
+  } else if (choiceValue <= 66) {
+    return 'PAPER';
+  } else {
+    return 'SCISSORS';
+  }
 }
 
-function alertWin (score) {
-    if (score === 5) {
-        if (score === playerScore) {
-            alert("Player Wins!");
-        } else if (score === computerScore) {
-            alert("Computer Wins!");
-        }
-    }
-} 
+function updateScoreDisplay() {
+  const playerScoreDisplay = document.querySelector('#playerScore');
+  playerScoreDisplay.innerHTML = `Player: ${playerScore}`;
 
+  const computerScoreDisplay = document.querySelector('#computerScore');
+  computerScoreDisplay.innerHTML = `Computer: ${computerScore}`;
+
+  const tieRecordDisplay = document.querySelector('#tieRecord');
+  tieRecordDisplay.innerHTML = `Rounds Tied: ${tieRecord}`;
+}
+
+// Checks the score to see if either player has reached 5 points.
+// If so, it displays a modal with the winner and a restart button.
 function checkScore() {
-    if (playerScore >= 5) {
-      const modal = document.getElementById("myModal");
-      const modalMessage = document.getElementById("modalMessage");
-      modalMessage.textContent = "Congratulations! You reached 5 points, and won the game.";
-      modal.style.display = "block";
-    } else if (computerScore >= 5) {
-      const modal = document.getElementById("myModal");
-      const modalMessage = document.getElementById("modalMessage");
-      modalMessage.textContent = "Gamer Over! The computer reached 5 points!";
-      modal.style.display = "block";
-    }
-  }
-  
-  document.getElementById("openModal").addEventListener("click", checkScore);
-  document.getElementById("restartButton").addEventListener("click", function() {
-    document.getElementById("myModal").style.display = "none";
-    restartGame();
-  });
+  if (playerScore >= 5 || computerScore >= 5) {
+    const modal = document.getElementById('myModal');
+    const modalMessage = document.getElementById('modalMessage');
 
-  function restartGame() {
-    playerScore = 0;
-    computerScore = 0;
-    tieRecord = 0;
-    let playerScoreDisplay = document.querySelector('#playerScore');
-    playerScoreDisplay.innerHTML = "Player: " + playerScore;
-    let computerScoreDisplay = document.querySelector('#computerScore');
-    computerScoreDisplay.innerHTML = "Computer: " + computerScore;
-    let tieRecordDisplay = document.querySelector('#tieRecord');
-    tieRecordDisplay.innerHTML = "Rounds Tied: " + tieRecord;
+    if (playerScore >= 5) {
+      modalMessage.textContent = 'Congratulations! You reached 5 points and won the game.';
+    } else {
+      modalMessage.textContent = 'Game Over! The computer reached 5 points!';
+    }
+
+    modal.style.display = 'block';
   }
+}
+
+document.getElementById('restartButton').addEventListener('click', () => {
+  document.getElementById('myModal').style.display = 'none';
+  restartGame();
+});
+
+function restartGame() {
+  playerScore = 0;
+  computerScore = 0;
+  tieRecord = 0;
+  updateScoreDisplay();
+}
