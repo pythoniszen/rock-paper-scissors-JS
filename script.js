c6f4105094e0dfe9a2bbc322f74d3f4c3908cb9b
Nathan <pythoniszen@gmail.com>
2023-06-02 18:31:54 -0700

    6/2/23 Add images for ui, add function to alert player when game is lost or won, and add function to restart game when game is over

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+let playerScore = 0;
+let computerScore = 0;
+let tieRecord = 0;
+
 game();
 
 function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    let tieRecord = 0;
+    
     let roundResult = '';
 
     const rock = document.querySelector('#rock');
@@ -11,6 +13,8 @@ function game() {
     rock.addEventListener('click', () => {
         roundResult = playRound(rock.id);
         console.log(roundResult);
+
+        
 
         if (roundResult.charAt(4) === 'W') {
             roundResult = 'Player Wins';
@@ -20,16 +24,19 @@ function game() {
             roundResult = 'Tie';
         }
 
+        
+
         if (roundResult === 'Player Wins') {
             playerScore++;
             console.log(playerScore);
             let playerScoreDisplay = document.querySelector('#playerScore');
             playerScoreDisplay.innerHTML = "Player: " + playerScore;
-            
+            checkScore();
         } else if (roundResult === 'Computer Wins') {
             computerScore++;
             let computerScoreDisplay = document.querySelector('#computerScore');
             computerScoreDisplay.innerHTML = "Computer: " + computerScore;
+            checkScore();
         } else if (roundResult === 'Tie') {
             tieRecord++;
             let tieRecordDisplay = document.querySelector('#tieRecord');
@@ -65,6 +72,8 @@ function playRound(elementId) {
     }
 
     return result;
+
+    
 }
 
 
@@ -78,3 +87,45 @@ function getComputerChoice() {
         return 'SCISSORS';
     }
 }
+
+function alertWin (score) {
+    if (score === 5) {
+        if (score === playerScore) {
+            alert("Player Wins!");
+        } else if (score === computerScore) {
+            alert("Computer Wins!");
+        }
+    }
+} 
+
+function checkScore() {
+    if (playerScore >= 5) {
+      const modal = document.getElementById("myModal");
+      const modalMessage = document.getElementById("modalMessage");
+      modalMessage.textContent = "Congratulations! You reached 5 points, and won the game.";
+      modal.style.display = "block";
+    } else if (computerScore >= 5) {
+      const modal = document.getElementById("myModal");
+      const modalMessage = document.getElementById("modalMessage");
+      modalMessage.textContent = "Gamer Over! The computer reached 5 points!";
+      modal.style.display = "block";
+    }
+  }
+  
+  document.getElementById("openModal").addEventListener("click", checkScore);
+  document.getElementById("restartButton").addEventListener("click", function() {
+    document.getElementById("myModal").style.display = "none";
+    restartGame();
+  });
+
+  function restartGame() {
+    playerScore = 0;
+    computerScore = 0;
+    tieRecord = 0;
+    let playerScoreDisplay = document.querySelector('#playerScore');
+    playerScoreDisplay.innerHTML = "Player: " + playerScore;
+    let computerScoreDisplay = document.querySelector('#computerScore');
+    computerScoreDisplay.innerHTML = "Computer: " + computerScore;
+    let tieRecordDisplay = document.querySelector('#tieRecord');
+    tieRecordDisplay.innerHTML = "Rounds Tied: " + tieRecord;
+  }
